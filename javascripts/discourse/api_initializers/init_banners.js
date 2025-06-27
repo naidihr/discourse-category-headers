b4a909dbecef5cc06ce92eba6fb0bd5b12251ea5
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-06-27 13:07:09 +0800

    Delete javascripts/discourse/api_initializers/init_banners.js

--- javascripts/discourse/api_initializers/init_banners.js
+++
@@ -1,8 +0,0 @@
-/*
-import { apiInitializer } from "discourse/lib/api";
-import CategoryHeader from "../components/category-header";
-
-export default apiInitializer((api) => {
-  api.renderInOutlet("above-category-heading", CategoryHeader);
-});
-*/
