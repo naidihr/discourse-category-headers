72b36024668c860a21a50fdb45bc6d68d1f3aab6
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-06-27 14:37:40 +0800

    Update and rename init_banners.js to init_banners.gjs

--- javascripts/discourse/api_initializers/init_banners.js
+++
@@ -1,7 +0,0 @@
-import { apiInitializer } from "discourse/lib/api";
-import CategoryHeader from "../components/category-header";
-import { tracked } from "@glimmer/tracking";
-
-export default apiInitializer((api) => {
-  api.renderInOutlet("above-main-container", CategoryHeader);
-});
