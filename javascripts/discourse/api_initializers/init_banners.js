16d22d7a926e089f65333a289f5910a21d01f821
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-06-27 12:54:06 +0800

    Update init_banners.js

--- javascripts/discourse/api_initializers/init_banners.js
+++ javascripts/discourse/api_initializers/init_banners.js
@@ -1,6 +1,8 @@
+/*
 import { apiInitializer } from "discourse/lib/api";
 import CategoryHeader from "../components/category-header";
 
 export default apiInitializer((api) => {
   api.renderInOutlet("above-category-heading", CategoryHeader);
 });
+*/
