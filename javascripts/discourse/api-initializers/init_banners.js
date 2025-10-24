702dcb91bd9fc01c5d1d964ebfe2cbf61d58e9f8
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-10-24 09:55:47 +0800

    Enhance init_banners with tracking and logging
    Added tracking for full category description and logging for page changes.

--- javascripts/discourse/api-initializers/init_banners.js
+++ javascripts/discourse/api-initializers/init_banners.js
@@ -1,6 +1,12 @@
+import { tracked } from "@glimmer/tracking";
+
 import { apiInitializer } from "discourse/lib/api";
 import CategoryHeader from "../components/category-header";
 
 export default apiInitializer((api) => {
+  @tracked full_category_description = "";
+  api.onPageChange((url) => {
+    console.log(url);
+  });
   api.renderInOutlet("above-category-heading", CategoryHeader);
 });
